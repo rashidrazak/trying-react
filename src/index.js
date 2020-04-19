6511461c5bd6a3799db6909ebc74d07cc9e42dc7
Rashid Razak <bib.nexus@gmail.com>
2020-04-19 08:30:11 +0800

    Add simple comment UI block

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 const App = () => {
   return (
-    <div>
-      <p>Rashid Razak was here!</p>
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>He's Awesome!</button>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Rashid
+          </a>
+          <div className="metadata">
+            <span className="date">
+              {new Date().toLocaleTimeString()}
+            </span>
+          </div>
+          <div className="text">Now Playing: Maroon 5 - Sugar</div>
+        </div>
+      </div>
     </div>
   )
 }
